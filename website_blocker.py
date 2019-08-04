ac6417fd0186606e77ae9c1d42d490d219879c00
Romana Rasheed <rasheed.romana@yahoo.com>
2019-08-04 12:43:04 -0500

    First_Draft

---
+++ website_blocker.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime as dt
+
+hosts_path ="/etc/hosts"
+redirect="127.0.0.1"
+website_list=["www.facebook.com","facebook.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print("Studying Hours...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            print(content)
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+ "     " +website+"\n")
+    else:
+        with open(hosts_path,'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun Hours...")
+    time.sleep(5) #the code keeps checking the time every 5 seconds
